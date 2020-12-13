23e76e0bf13b087fb58b69665c2bf1dceb426563
Marc Chalain <marc.chalain@gmail.com>
2020-12-13 21:15:19 +0100

    [export] use thread to send events

--- src/export.c
+++ src/export.c
@@ -35,21 +35,38 @@
 #include <sys/stat.h>
 #include <sched.h>
 #include <sys/wait.h>
+#include <pthread.h>
 
 #include "log.h"
 #include "gpiomonitor.h"
 
 typedef struct export_s export_t;
-typedef int (*export_format_t)(const export_t *ctx, int chipid, int gpioid, struct gpiod_line_event *event);
+typedef struct export_event_s export_event_t;
+
+struct export_event_s
+{
+	struct gpiod_line_event event;
+	int chipid;
+	int gpioid;
+	struct export_event_s *next;
+};
+
+typedef int (*export_format_t)(const export_t *ctx, int socket, export_event_t *event);
 struct export_s
 {
 	int socket;
 	export_format_t format;
 	int rootfd;
-	const char *url;
+	char *url;
+	int run;
+	export_event_t *events;
+	pthread_t thread;
+	pthread_mutex_t mutex;
+	pthread_cond_t cond;
+
 };
 
-static int export_format_raw(const export_t *ctx, int chipid, int gpioid, struct gpiod_line_event *event);
+static int export_format_raw(const export_t *ctx, int socket, export_event_t *event);
 
 static const char str_rising[] = "rising";
 static const char str_falling[] = "falling";
@@ -59,6 +76,24 @@ static const char str_GPIOENV[] = "GPIO=%.2d";
 static const char str_CHIPENV[] = "CHIP=%.2d";
 static const char str_NAMEENV[] = "NAME=%s";
 static const char str_ACTIONENV[] = "ACTION=%s     ";
+
+static void *export_thread(void * arg)
+{
+	export_t *ctx = (export_t *)arg;
+	while (ctx->run)
+	{
+		pthread_mutex_lock(&ctx->mutex);
+		while (ctx->events == NULL)
+			pthread_cond_wait(&ctx->cond, &ctx->mutex);
+		while (ctx->events != NULL)
+		{
+			ctx->format(ctx, ctx->socket, ctx->events);
+			ctx->events = ctx->events->next;
+		}
+		pthread_mutex_unlock(&ctx->mutex);
+	}
+	return NULL;
+}
 
 void *export_create(int rootfd, const char *url, const char *format)
 {
@@ -75,25 +110,36 @@ void *export_create(int rootfd, const char *url, const char *format)
 			free(ctx);
 			return NULL;
 		}
-		ctx->url = url;
+		ctx->url = strdup(url);
 	}
 	if (!strcmp(format, "raw"))
 	{
 		ctx->format = &export_format_raw;
 	}
+	pthread_cond_init(&ctx->cond, NULL);
+	pthread_mutex_init(&ctx->mutex, NULL);
+	pthread_create(&ctx->thread, NULL, export_thread, ctx);
+	pthread_detach(ctx->thread);
 	return ctx;
 }
 
-static int export_format_raw(const export_t *ctx, int chipid, int gpioid, struct gpiod_line_event *event)
+static int export_format_raw(const export_t *ctx, int socket, export_event_t *event)
 {
-	return write(ctx->socket, event, sizeof(*event));
+	return write(socket, &event->event, sizeof(event->event));
 }
 
 void export_run(void *arg, int chipid, int gpioid, struct gpiod_line_event *event)
 {
-	const export_t *ctx = (const export_t *)arg;
-	if (ctx->format != NULL)
-		ctx->format(ctx, chipid, gpioid, event);
+	export_t *ctx = (export_t *)arg;
+
+	pthread_mutex_lock(&ctx->mutex);
+	export_event_t *new = calloc(1, sizeof(*new));
+	memcpy(&new->event, event, sizeof(new->event));
+	new->next = ctx->events;
+	ctx->events = new;
+	pthread_mutex_unlock(&ctx->mutex);
+
+	pthread_cond_broadcast(&ctx->cond);
 	return;
 }
 
@@ -101,9 +147,12 @@ void export_free(void *arg)
 {
 	export_t *ctx = (export_t *)arg;
 	close(ctx->socket);
+	pthread_cond_destroy(&ctx->cond);
+	pthread_mutex_destroy(&ctx->mutex);
 	if (!strncmp(ctx->url, "fifo://", 7))
 	{
 		unlinkat(ctx->rootfd, ctx->url + 7, 0);
 	}
+	free(ctx->url);
 	free(ctx);
 }
